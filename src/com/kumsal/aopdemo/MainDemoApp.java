3476ca21fdb47e7c93b0b8484b10e93d6ffb891e
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-11-17 16:05:23 +0300

    add Account element

--- src/com/kumsal/aopdemo/MainDemoApp.java
+++ src/com/kumsal/aopdemo/MainDemoApp.java
@@ -2,11 +2,18 @@ package com.kumsal.aopdemo;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import com.kumsal.aopdemo.doa.AccountDAO;
+
 public class MainDemoApp {
 
 	public static void main(String[] args) {
 		AnnotationConfigApplicationContext x=new AnnotationConfigApplicationContext(DemoConfig.class);
 		
+		AccountDAO theAccountDao=x.getBean("accountDAO", AccountDAO.class);
+		
+		theAccountDao.addAccount();
+		
+		x.close();
 
 	}
 
