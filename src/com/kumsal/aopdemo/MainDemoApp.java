0769678978ba54d3bf45cb92556ae267628dfc68
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-11-17 16:01:18 +0300

    Add new config elemnt

---
+++ src/com/kumsal/aopdemo/MainDemoApp.java
@@ -0,0 +1,13 @@
+package com.kumsal.aopdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MainDemoApp {
+
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext x=new AnnotationConfigApplicationContext(DemoConfig.class);
+		
+
+	}
+
+}
