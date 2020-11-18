bb16e82d24b0227a9d28070bd8d228baba52363c
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-11-18 13:20:11 +0300

    add new member line

--- src/com/kumsal/aopdemo/MainDemoApp.java
+++ src/com/kumsal/aopdemo/MainDemoApp.java
@@ -3,6 +3,7 @@ package com.kumsal.aopdemo;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.kumsal.aopdemo.doa.AccountDAO;
+import com.kumsal.aopdemo.doa.MamberShipDAO;
 
 public class MainDemoApp {
 
@@ -12,8 +13,11 @@ public class MainDemoApp {
 		
 		AccountDAO theAccountDao=context.getBean("accountDAO", AccountDAO.class);
 		
+		MamberShipDAO theMemberShipDAO=context.getBean("mamberShipDAO",MamberShipDAO.class);
+		
 		theAccountDao.addAccount();
 		
+		theMemberShipDAO.addAccount();
 		context.close();
 
 	}
