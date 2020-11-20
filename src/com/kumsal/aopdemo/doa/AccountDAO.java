9e752ba34585a4acaf00a8be43d81a77d12ffee9
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-11-20 17:04:30 +0300

    Add account parameter

--- src/com/kumsal/aopdemo/doa/AccountDAO.java
+++ src/com/kumsal/aopdemo/doa/AccountDAO.java
@@ -2,10 +2,12 @@ package com.kumsal.aopdemo.doa;
 
 import org.springframework.stereotype.Component;
 
+import com.kumsal.aopdemo.Account;
+
 @Component
 public class AccountDAO {
 
-	public void addAccount(){
+	public void addAccount(Account theAccount){
 		System.out.println(getClass()+" :DOING MY DB WORK: ADDING AN ACCOUNT");
 	}
 }
