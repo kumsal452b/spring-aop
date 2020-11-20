a99c3f1b2e75a86ee70a9d6b372b1c125b7681d9
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-11-20 17:15:47 +0300

    add new parameter from app class

--- src/com/kumsal/aopdemo/doa/AccountDAO.java
+++ src/com/kumsal/aopdemo/doa/AccountDAO.java
@@ -7,7 +7,9 @@ import com.kumsal.aopdemo.Account;
 @Component
 public class AccountDAO {
 
-	public void addAccount(Account theAccount){
+	public void addAccount(Account theAccount,boolean vipFlag){
+		
 		System.out.println(getClass()+" :DOING MY DB WORK: ADDING AN ACCOUNT");
+		
 	}
 }
