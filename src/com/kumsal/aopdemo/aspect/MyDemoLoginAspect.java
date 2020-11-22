dad86f9317597f3947bc02bddd70b43d4894bffb
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-11-22 16:32:45 +0300

    add class from advice

--- src/com/kumsal/aopdemo/aspect/MyDemoLoginAspect.java
+++ src/com/kumsal/aopdemo/aspect/MyDemoLoginAspect.java
@@ -2,13 +2,19 @@ package com.kumsal.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyDemoLoginAspect {
 
-	@Before("execution(* com.kumsal.aopdemo.doa.*.*( ..))")
+	@Pointcut("execution(* com.kumsal.aopdemo.doa.*.*( ..))")
+	private void forDaoPacket(){
+		
+	}
+	
+	@Before("forDaoPacket()")
 	public void beforeAddAccountAdvice(){
 		System.out.println("=======> Executing @Before advice on addAccount()");
 	}
