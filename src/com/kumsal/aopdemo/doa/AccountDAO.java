a934e12078ebd0a6f8c1550edd190d1b91bd7d6c
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-11-22 16:51:28 +0300

    add  new fıeld and getter and setter

--- src/com/kumsal/aopdemo/doa/AccountDAO.java
+++ src/com/kumsal/aopdemo/doa/AccountDAO.java
@@ -7,6 +7,9 @@ import com.kumsal.aopdemo.Account;
 @Component
 public class AccountDAO {
 
+	private String name;
+	private String serviceCode;
+	
 	public void addAccount(Account theAccount,boolean vipFlag){
 		
 		System.out.println(getClass()+" :DOING MY DB WORK: ADDING AN ACCOUNT");
@@ -16,4 +19,17 @@ public class AccountDAO {
 		System.out.println(getClass()+" :doing work");
 		return false;
 	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getServiceCode() {
+		return serviceCode;
+	}
+	public void setServiceCode(String serviceCode) {
+		this.serviceCode = serviceCode;
+	}
+	
 }
