ed54c2bd464f1f13df1c2341eae08d0974198a61
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-11-25 14:56:02 +0300

    add new paht element

--- src/com/kumsal/aopdemo/aspect/MyCloudAsyncAspect.java
+++ src/com/kumsal/aopdemo/aspect/MyCloudAsyncAspect.java
@@ -2,10 +2,12 @@ package com.kumsal.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(1)
 public class MyCloudAsyncAspect {
 
 	@Before("com.kumsal.aopdemo.aspect.LuvAopExpressions.forDaoPackacgeNoSetterAndGetter()")
