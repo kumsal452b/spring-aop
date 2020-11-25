caac36f8b55db4e696f56c4e90f851b925bb0c42
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-11-25 14:05:55 +0300

    paste in he method body

--- src/com/kumsal/aopdemo/aspect/MyAnalyticsAspect.java
+++ src/com/kumsal/aopdemo/aspect/MyAnalyticsAspect.java
@@ -1,10 +1,15 @@
 package com.kumsal.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyAnalyticsAspect {
-
+	@Before("forDaoPackacgeNoSetterAndGetter()")
+	public void performApiAnalitycs(){
+		System.out.println("Performing api");
+	}
+	
 }
