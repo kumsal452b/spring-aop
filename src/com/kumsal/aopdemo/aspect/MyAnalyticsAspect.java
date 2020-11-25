f2f5c22d873d535dab2ceed952e110694909e47b
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-11-25 14:55:45 +0300

    add packacge name

--- src/com/kumsal/aopdemo/aspect/MyAnalyticsAspect.java
+++ src/com/kumsal/aopdemo/aspect/MyAnalyticsAspect.java
@@ -2,10 +2,12 @@ package com.kumsal.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(3)
 public class MyAnalyticsAspect {
 	@Before("com.kumsal.aopdemo.aspect.LuvAopExpressions.forDaoPackacgeNoSetterAndGetter()")
 	public void performApiAnalitycs(){
