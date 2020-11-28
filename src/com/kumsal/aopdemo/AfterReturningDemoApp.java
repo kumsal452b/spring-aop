7705b6788ccbc98e83f6e746aed36d429a4db60a
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-11-28 12:45:24 +0300

    add demo class

---
+++ src/com/kumsal/aopdemo/AfterReturningDemoApp.java
@@ -0,0 +1,34 @@
+package com.kumsal.aopdemo;
+
+import java.util.Scanner;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.kumsal.aopdemo.doa.AccountDAO;
+import com.kumsal.aopdemo.doa.MamberShipDAO;
+
+public class AfterReturningDemoApp {
+
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext context = 
+				   new AnnotationConfigApplicationContext(DemoConfig.class);
+		
+		AccountDAO theAccountDao=context.getBean("accountDAO", AccountDAO.class);
+		
+		MamberShipDAO theMemberShipDAO=context.getBean("mamberShipDAO",MamberShipDAO.class);
+		Account myAccount=new Account();
+		
+		myAccount.setName("Kumsal");
+		myAccount.setLevel("Platinium");
+		
+		theAccountDao.addAccount(myAccount,true);
+		theAccountDao.doWork();
+		theAccountDao.setName("foobar");
+		theAccountDao.setServiceCode("silver");
+		
+		theMemberShipDAO.addAccount();
+		theMemberShipDAO.addAccount();
+		context.close();
+	}
+
+}
