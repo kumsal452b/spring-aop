cdfb6953e7fc10a1cf72355f0b54fd13600e3c74
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-11-29 12:23:38 +0300

    add new  after class

---
+++ src/com/kumsal/aopdemo/AfterThrowingDemoApp.java
@@ -0,0 +1,27 @@
+package com.kumsal.aopdemo;
+
+import java.util.List;
+import java.util.Scanner;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.kumsal.aopdemo.doa.AccountDAO;
+import com.kumsal.aopdemo.doa.MamberShipDAO;
+
+public class AfterThrowingDemoApp {
+
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext context = 
+				   new AnnotationConfigApplicationContext(DemoConfig.class);
+		
+		AccountDAO theAccountDao=context.getBean("accountDAO", AccountDAO.class);
+		List<Account> theAccounts=theAccountDao.findAcounts();
+		
+		System.out.println("Main progaram afterReturning");
+		System.out.println(theAccounts);
+		System.out.println("end of list ");
+		
+		context.close();
+	}
+
+}
