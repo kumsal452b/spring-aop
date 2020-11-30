3664af24521d432557f27dafe63886434693c3a1
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-11-30 14:13:45 +0300

    add new method from class

--- src/com/kumsal/aopdemo/service/TrafficFortuneService.java
+++ src/com/kumsal/aopdemo/service/TrafficFortuneService.java
@@ -1,5 +1,13 @@
 package com.kumsal.aopdemo.service;
 
+import java.util.concurrent.TimeUnit;
+
 public class TrafficFortuneService {
 
+	public String getFortune() throws InterruptedException{
+		TimeUnit.SECONDS.sleep(5);
+	
+		
+		return "Expect heavy this morning";
+	}
 }
